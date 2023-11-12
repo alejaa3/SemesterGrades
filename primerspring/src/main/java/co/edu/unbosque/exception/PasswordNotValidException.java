bf55f7f00f558615c89af7ebba1c6ba3ff671a0a
alejaa3 <141250722+alejaa3@users.noreply.github.com>
2023-11-11 21:40:41 -0500

    Delete primerspring directory

--- primerspring/src/main/java/co/edu/unbosque/exception/PasswordNotValidException.java
+++
@@ -1,9 +0,0 @@
-package co.edu.unbosque.exception;
-
-public class PasswordNotValidException extends Exception{
-
-	public PasswordNotValidException() {
-		super("The password does not meet the standard. It should have at least: 8 characters, a number, an uppercase character, a lowercase character and an especial character");
-	}
-	
-}
